6fd3cc71b5e746c36bd640b81ce181d267cc716d
Jabril Abdilahi <94321684+Jabrilthecoder@users.noreply.github.com>
2022-06-22 14:26:38 -0500

    Add files via upload

---
+++ memory-game.js
@@ -0,0 +1,35 @@
+  const cardArray = [
+      {
+          name: 'blank',
+          img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/blank.png' ,
+      },
+      {
+        name: 'cheeseburger',
+        img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/cheeseburger.png' ,
+    },
+    {
+        name: 'fries',
+        img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/fries.png' ,
+    },
+    {
+        name: 'pizza',
+        img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/pizza.png' ,
+    },
+    {
+        name: 'hotdog',
+        img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/hotdog.png' ,
+    },
+    {
+        name: 'milkshake',
+        img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/milkshake.png' ,
+    },
+    {
+        name: 'icecream',
+        img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/icecream.png' ,
+    },
+    {
+        name: 'Blank2',
+        img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/white.png' ,
+    }
+
+  ]
